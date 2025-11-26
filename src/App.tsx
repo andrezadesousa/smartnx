cf0253a4e85269aed243f0c9bb9deb37f0bbb821
Andreza Sousa <tech.front1@cali.li>
2025-11-26 17:10:44 -0300

    unnecessary files removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Layout } from "antd";
+import CharacterList from "./components/CharacterList";
+import "antd/dist/antd.css";
+
+const { Header, Content } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout style={{ minHeight: "100vh" }}>
+      <Header style={{ color: "#fff", fontSize: 20 }}>
+        SWAPI - Personagens
+      </Header>
+      <Content style={{ padding: "24px" }}>
+        <CharacterList />
+      </Content>
+    </Layout>
   );
 }
 
