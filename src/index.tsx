c73e54ef0e96aca099fe72bef8a271a88f7bab44
Andreza Sousa <tech.front1@cali.li>
2025-11-29 14:41:19 -0300

    style/New colors added

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { ConfigProvider } from "antd";
 import "antd/dist/antd.css";
-import "./styles/theme.css";
+import "./themes/style.css";
 
 ReactDOM.render(
   <React.StrictMode>
